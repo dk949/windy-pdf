27ef11b5073d45220b685e434b601a57b5d0ce97
dk949 <56653556+dk949@users.noreply.github.com>
2024-08-21 19:29:14 +0100

    removed the need to check if `tw` is set on every call

--- src/index.tsx
+++ src/index.tsx
@@ -16,41 +16,37 @@ type OmitWithUnion<T, K extends keyof T> = T extends any
 type WindyProps<T extends keyof JSX.IntrinsicElements | JSXElementConstructor<any>>
     = OmitWithUnion<ComponentProps<T> & { className?: string }, "style">;
 
-let tw: ((_: string) => Style) | undefined = undefined;
+let tw: ((_: string) => Style) = createTw({
+    content: [],
+    theme: { extend: {}, },
+    plugins: [],
+});
+
 export function setup(config: Config) {
     tw = createTw(config);
 }
 
-
 export function Document({ className, ...props }: WindyProps<typeof Pdf.Document>) {
-    if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
     return (<Pdf.Document style={className ? tw(className) : undefined} {...props} />);
 };
 export function Page({ className, ...props }: WindyProps<typeof Pdf.Page>) {
-    if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
     return (<Pdf.Page style={className ? tw(className) : undefined} {...props} />);
 };
 export function View({ className, ...props }: WindyProps<typeof Pdf.View>) {
-    if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
     return (<Pdf.View style={className ? tw(className) : undefined} {...props} />);
 };
 export function Image({ className, ...props }: WindyProps<typeof Pdf.Image>) {
-    if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
     return (<Pdf.Image style={className ? tw(className) : undefined} {...props} />);
 };
 export function Text({ className, ...props }: WindyProps<typeof Pdf.Text>) {
-    if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
     return (<Pdf.Text style={className ? tw(className) : undefined} {...props} />);
 };
 export function Link({ className, ...props }: WindyProps<typeof Pdf.Link>) {
-    if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
     return (<Pdf.Link style={className ? tw(className) : undefined} {...props} />);
 };
 export function Note({ className, ...props }: WindyProps<typeof Pdf.Note>) {
-    if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
     return (<Pdf.Note style={className ? tw(className) : undefined} {...props} />);
 };
 export function Canvas({ className, ...props }: WindyProps<typeof Pdf.Canvas>) {
-    if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
     return (<Pdf.Canvas style={className ? tw(className) : undefined} {...props} />);
 };
