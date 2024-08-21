d13382244655e36bb01c063db6c1e6f84ccfaa1f
dk949 <56653556+dk949@users.noreply.github.com>
2024-08-21 19:25:47 +0100

    use types to disallow style to be passed as a prop

--- src/index.tsx
+++ src/index.tsx
@@ -6,52 +6,51 @@ import type { ComponentProps, JSXElementConstructor } from "react";
 
 import type { Config } from 'tailwindcss'
 
+// By putting `T` in a conditional, `Omit` gets distributed over the subtypes of
+// `T`, rather than being applied to `T` as a whole.
+type OmitWithUnion<T, K extends keyof T> = T extends any
+    ? Omit<T, K>
+    : never;
+
 
 type WindyProps<T extends keyof JSX.IntrinsicElements | JSXElementConstructor<any>>
-    = ComponentProps<T> & { className?: string };
+    = OmitWithUnion<ComponentProps<T> & { className?: string }, "style">;
 
 let tw: ((_: string) => Style) | undefined = undefined;
 export function setup(config: Config) {
     tw = createTw(config);
 }
 
-export function Document({ className, style, ...props }: WindyProps<typeof Pdf.Document>) {
+
+export function Document({ className, ...props }: WindyProps<typeof Pdf.Document>) {
     if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
-    if (style != null) throw new Error("Cannot pass style to windified component");
     return (<Pdf.Document style={className ? tw(className) : undefined} {...props} />);
 };
-export function Page({ className, style, ...props }: WindyProps<typeof Pdf.Page>) {
+export function Page({ className, ...props }: WindyProps<typeof Pdf.Page>) {
     if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
-    if (style != null) throw new Error("Cannot pass style to windified component");
     return (<Pdf.Page style={className ? tw(className) : undefined} {...props} />);
 };
-export function View({ className, style, ...props }: WindyProps<typeof Pdf.View>) {
+export function View({ className, ...props }: WindyProps<typeof Pdf.View>) {
     if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
-    if (style != null) throw new Error("Cannot pass style to windified component");
     return (<Pdf.View style={className ? tw(className) : undefined} {...props} />);
 };
-export function Image({ className, style, ...props }: WindyProps<typeof Pdf.Image>) {
+export function Image({ className, ...props }: WindyProps<typeof Pdf.Image>) {
     if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
-    if (style != null) throw new Error("Cannot pass style to windified component");
     return (<Pdf.Image style={className ? tw(className) : undefined} {...props} />);
 };
-export function Text({ className, style, ...props }: WindyProps<typeof Pdf.Text>) {
+export function Text({ className, ...props }: WindyProps<typeof Pdf.Text>) {
     if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
-    if (style != null) throw new Error("Cannot pass style to windified component");
     return (<Pdf.Text style={className ? tw(className) : undefined} {...props} />);
 };
-export function Link({ className, style, ...props }: WindyProps<typeof Pdf.Link>) {
+export function Link({ className, ...props }: WindyProps<typeof Pdf.Link>) {
     if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
-    if (style != null) throw new Error("Cannot pass style to windified component");
     return (<Pdf.Link style={className ? tw(className) : undefined} {...props} />);
 };
-export function Note({ className, style, ...props }: WindyProps<typeof Pdf.Note>) {
+export function Note({ className, ...props }: WindyProps<typeof Pdf.Note>) {
     if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
-    if (style != null) throw new Error("Cannot pass style to windified component");
     return (<Pdf.Note style={className ? tw(className) : undefined} {...props} />);
 };
-export function Canvas({ className, style, ...props }: WindyProps<typeof Pdf.Canvas>) {
+export function Canvas({ className, ...props }: WindyProps<typeof Pdf.Canvas>) {
     if (tw == null) throw new Error("You need to call `setup` prior to using any of the components");
-    if (style != null) throw new Error("Cannot pass style to windified component");
     return (<Pdf.Canvas style={className ? tw(className) : undefined} {...props} />);
 };
